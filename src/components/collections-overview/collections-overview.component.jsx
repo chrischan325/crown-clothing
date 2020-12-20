d23c494912862f983c41dd6726126a3117556564
Christian Chan <55823764+chrischan325@users.noreply.github.com>
2020-12-20 02:42:46 -0500

    updated selectors and components for ux flow for directory/shop

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import "./collections-overview.styles.scss";
 import { createStructuredSelector } from "reselect";
 import CollectionPreview from "../../components/preview-collection/collection-preview";
-import { selectCollections } from '../../redux/shop/shop.selectors';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
 const CollectionsOverview = ({ collections }) => (
   <div className="collections-overview">
@@ -14,7 +14,7 @@ const CollectionsOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
+    collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
